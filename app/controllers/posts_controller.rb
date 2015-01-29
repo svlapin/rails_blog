f19bf58d40762c7a0b82a8d6a313e6a160d6d78a
Sergey Lapin <sv0lapin@gmail.com>
2015-01-29 16:57:20 +0300

    validation of models, form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,10 +4,21 @@ class PostsController < ApplicationController
   end
 
   def new
-
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
 
+    if @post.save
+      redirect_to posts_path
+    else
+      render action: 'new'
+    end
   end
+
+  private
+    def post_params
+      params.require(:post).permit(:body)
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
   has_many :posts, dependent: :destroy
+
+  validates :login, presence: true, length: { minimum: 3 }
 end
