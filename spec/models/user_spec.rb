81bfe1a00a36ead91ca14c22ebef798ba2f1e8be
Sergey Lapin <sv0lapin@gmail.com>
2015-01-30 14:46:32 +0300

    user auth test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,11 @@ require 'rails_helper'
 RSpec.describe User, :type => :model do
   let(:user) { FactoryGirl.create(:user) }
 
-  it 'is valid' do 
+  it 'is valid' do
     expect(user).to be_valid
+  end
+
+  it 'is authenticated properly' do
+    expect(user.authenticate(user.password)).to eq(true)
   end
 end
